8853ee5aa040f1e13d39ea3692e1a517aeb5159e
MANIKAAGARG <143868417+MANIKAAGARG@users.noreply.github.com>
2025-04-13 23:50:06 +0530

    Update mainfile.cpp

--- mainfile.cpp
+++ mainfile.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include "WindowSwitcher.h"
+using namespace std;
 
 int main() {
     WindowSwitcher switcher;
-    std::string command;
+    string command;
 
-    std::cout << "=== Window Switcher (C++ DSA Project) ===\n";
+    cout << "=== Window Switcher (C++ DSA Project) ===\n";
 
     while (true) {
-        std::cout << "\nOptions: open, next, prev, close, reopen, current, exit\n>> ";
-        std::cin >> command;
+        cout << "\nOptions: open, next, prev, close, reopen, current, exit\n>> ";
+        cin >> command;
 
         if (command == "open") {
-            std::string app;
-            std::cout << "Enter app name: ";
-            std::cin.ignore(); // clear buffer
-            std::getline(std::cin, app);
+            string app;
+            cout << "Enter app name: ";
+            cin.ignore(); // clear buffer
+            getline(cin, app);
             switcher.openWindow(app);
         } else if (command == "next") {
             switcher.switchNext();
@@ -28,10 +29,10 @@ int main() {
         } else if (command == "current") {
             switcher.displayCurrent();
         } else if (command == "exit") {
-            std::cout << "Exiting Window Switcher.\n";
+            cout << "Exiting Window Switcher.\n";
             break;
         } else {
-            std::cout << "Unknown command.\n";
+            cout << "Unknown command.\n";
         }
     }
 
